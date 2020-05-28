51bdaf03c055fe4d03e73a0cd7666e4b8f020e31
I407846 <o.aboudehn@student.fontys.nl>
2020-05-28 14:23:16 +0200

    create initial design ,add Classes (User, StudentHousing)

---
+++ StudentHousingCompany/StudentHousingCompany/FrmTenant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentHousingCompany
+{
+    public partial class FrmTenant : Form
+    {
+        public FrmTenant()
+        {
+            InitializeComponent();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            FrmAdmin admin = new FrmAdmin();
+            admin.Show();
+        }
+    }
+}
